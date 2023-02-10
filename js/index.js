a568b5071f52f62feae089656d31ffb3ce1ab5c6
clevertonx <clevertonx@gmail.com>
2023-02-10 10:41:13 -0400

    desaparecer fungo

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-/* objetivo 1 - quando o usuário clicar no botão de veja o trailer, devemos abrir a modal com o video do trailer
-objetivo 2 - quando o usuário clicar no X devemos fechar a modal
-
-objetivo 1 - quando o usuário clicar no botão de veja o trailer, devemos abrir a modal com o video do trailer
-
-- passo 1 - dar um jeito de pegar o elemento que representa o botão na tela usando o js
-- passo 2 - dar um jeito de identificar quando o usuário clicar no botão
-- passo 3 - dar um jeito de pegar o elemento da modal no js
-- passo 4 - abrir a modal na tela
-
-objetivo 2 - quando o usuário clicar no X devemos fechar a modal
-
-*/
-
 console.log('mostrar o document', document);
 
 const botaoTrailer = document.querySelector(".botao-trailer");
@@ -29,8 +15,8 @@ button.addEventListener("click", function () {
   currentIndex = (currentIndex + 1) % classes.length;
 });
 
-botaoClose.addEventListener("click", function() {
-  classes.forEach(function(className) {
+botaoClose.addEventListener("click", function () {
+  classes.forEach(function (className) {
     const element = document.querySelector(className);
     element.style.display = "none";
   });
